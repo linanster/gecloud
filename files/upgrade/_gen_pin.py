87d4d842f29baa948ab378e63d0616687b17e5fe
LiNan <believelinan@aliyun.com>
2020-05-25 11:17:48 +0800

    update _gen_pin.py: enhance pin string format

--- files/upgrade/_gen_pin.py
+++ files/upgrade/_gen_pin.py
@@ -1,5 +1,16 @@
-import uuid, sys
+from uuid import uuid4
+from datetime import datetime
+import sys
 
-pin = uuid.uuid4().hex
+version = sys.argv[1]
 
-print('{}-{}'.format(sys.argv[1], pin))
+timestr = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+
+pin = uuid4().hex[0:4]
+
+
+
+
+
+
+print('{}-{}-{}'.format(version, timestr, pin))
