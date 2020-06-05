bd10de769ae3360ec2ba72f1a875d61b9a7fb1a0
LiNan <believelinan@aliyun.com>
2020-06-05 15:52:12 +0800

    implement api/db/statistic/testdatacloud

--- app/apis/api_db.py
+++ app/apis/api_db.py
@@ -6,7 +6,7 @@ from app.models.mysql import Factory, Device, TestdataCloud
 from app.lib.myauth import http_basic_auth, my_login_required
 from app.lib.mydecorator import viewfunclog
 
-api_client_db = Api(prefix='/api/db/')
+api_db = Api(prefix='/api/db/')
 
 #####################################################################
 ### 1. fields definition, for marshal (custom object serializing) ###
@@ -92,7 +92,7 @@ parser.add_argument('dateend', type=str, location=['args', 'form'])
 ####################################
 
 
-class ResourceFactory(Resource):
+class ResourceFactoryRaw(Resource):
     # @http_basic_auth.login_required
     @my_login_required
     @viewfunclog
@@ -108,7 +108,7 @@ class ResourceFactory(Resource):
         }
         return response_obj
 
-class ResourceDevice(Resource):
+class ResourceDeviceRaw(Resource):
     # @http_basic_auth.login_required
     @my_login_required
     @viewfunclog
@@ -124,7 +124,43 @@ class ResourceDevice(Resource):
         }
         return response_obj
 
-class ResourceTestdataCloud(Resource):
+def query_testdatacloud(factorycode, devicecode, datestart_str, dateend_str):
+    myformat = '%Y-%m-%d'
+    datestart = datetime.strptime(datestart_str, myformat) if datestart_str is not None else None
+    dateend = datetime.strptime(dateend_str, myformat) + timedelta(days=1) if dateend_str is not None else None
+    if factorycode is None and devicecode is None:
+        if datestart_str is not None and dateend_str is not None:
+            data = TestdataCloud.query.filter(TestdataCloud.datetime.between(datestart, dateend)).all()
+        else:
+            data = TestdataCloud.query.all()
+    elif factorycode is not None and devicecode is not None:
+        if datestart_str is not None or dateend_str is not None:
+            data = TestdataCloud.query.filter_by(factorycode=factorycode, devicecode=devicecode).filter(TestdataCloud.datetime.between(datestart, dateend)).all()
+        else:
+            data = TestdataCloud.query.filter_by(factorycode=factorycode, devicecode=devicecode).all()
+    elif factorycode is not None and devicecode is None:
+        if datestart_str is not None and dateend_str is not None:
+            data = TestdataCloud.query.filter_by(factorycode=factorycode).filter(TestdataCloud.datetime.between(datestart, dateend)).all()
+        else:
+            data = TestdataCloud.query.filter_by(factorycode=factorycode).all()
+    elif  factorycode is None and devicecode is not None:
+        if datestart_str is not None and dateend_str is not None:
+            data = TestdataCloud.query.filter_by(devicecode=devicecode).filter(TestdataCloud.datetime.between(datestart, dateend)).all()
+        else:
+            data = TestdataCloud.query.filter_by(devicecode=devicecode).all()
+    else:
+        data = None
+    return data
+
+def filter_passed(data):
+    passed = list()
+    for item in data:
+        if item.bool_qualified_signal and item.bool_qualified_check and item.bool_qualified_scan and item.bool_qualified_deviceid and item.reserve_bool_1:
+            passed.append(item)
+    return passed
+
+
+class ResourceTestdataCloudRaw(Resource):
     # @http_basic_auth.login_required
     @my_login_required
     @viewfunclog
@@ -135,46 +171,57 @@ class ResourceTestdataCloud(Resource):
         devicecode = args.get('devicecode')
         datestart_str = args.get('datestart')
         dateend_str = args.get('dateend')
-        myformat = '%Y-%m-%d'
-        datestart = datetime.strptime(datestart_str, myformat) if datestart_str is not None else None
-        dateend = datetime.strptime(dateend_str, myformat) + timedelta(days=1) if dateend_str is not None else None
-        if factorycode is None and devicecode is None:
-            if datestart_str is not None and dateend_str is not None:
-                data = TestdataCloud.query.filter(TestdataCloud.datetime.between(datestart, dateend)).all()
-            else:
-                data = TestdataCloud.query.all()
-        elif factorycode is not None and devicecode is not None:
-            if datestart_str is None or dateend_str is None:
-                data = TestdataCloud.query.filter_by(factorycode=factorycode, devicecode=devicecode).filter(TestdataCloud.datetime.between(datestart, dateend)).all()
-            else:
-                data = TestdataCloud.query.filter_by(factorycode=factorycode, devicecode=devicecode).all()
-        elif factorycode is not None and devicecode is None:
-            if datestart_str is not None and dateend_str is not None:
-                data = TestdataCloud.query.filter_by(factorycode=factorycode).filter(TestdataCloud.datetime.between(datestart, dateend)).all()
-            else:
-                data = TestdataCloud.query.filter_by(factorycode=factorycode).all()
-        elif  factorycode is None and devicecode is not None:
-            if datestart_str is not None and dateend_str is not None:
-                data = TestdataCloud.query.filter_by(devicecode=devicecode).filter(TestdataCloud.datetime.between(datestart, dateend)).all()
-            else:
-                data = TestdataCloud.query.filter_by(devicecode=devicecode).all()
-        else:
-            data = None
+
+        data = query_testdatacloud(factorycode, devicecode, datestart_str, dateend_str)
+
         response_obj = {
             'status': 201,
             'count': len(data),
-            'msg': 'al the test data with factorycode {} devicecode {} between date {} and {}'.format(factorycode, devicecode, datestart_str, dateend_str),
+            'msg': 'all the test data with factorycode {} devicecode {} between date {} and {}'.format(factorycode, devicecode, datestart_str, dateend_str),
             'data': data
         }
         return response_obj
 
+class ResourceTestdataCloudStatistic(Resource):
+    # @http_basic_auth.login_required
+    @my_login_required
+    @viewfunclog
+    def get(self, metric):
+        args = parser.parse_args()
+        factorycode = args.get('factorycode')
+        devicecode = args.get('devicecode')
+        datestart_str = args.get('datestart')
+        dateend_str = args.get('dateend')
+        myformat = '%Y-%m-%d'
+        datestart = datetime.strptime(datestart_str, myformat) if datestart_str is not None else None
+        dateend = datetime.strptime(dateend_str, myformat) + timedelta(days=1) if dateend_str is not None else None
+
+        if metric == 'totalcount':
+            data = query_testdatacloud(factorycode, devicecode, datestart_str, dateend_str)
+            value = len(data)
+        elif metric == 'passrate':
+            total = query_testdatacloud(factorycode, devicecode, datestart_str, dateend_str)
+            passed = filter_passed(total)
+            totalcount = len(total)
+            passedcount = len(passed)
+            print('==totalcount==', totalcount)
+            print('==passedcount==', passedcount)
+            value = round(passedcount/totalcount, 4)
+        else:
+            value = 'error'
+        response_obj = {
+            'metric': metric,
+            'value': value,
+        }
+        return response_obj
 
 
 ##############################
 ### 4. Resourceful Routing ###
 ##############################
 
-api_client_db.add_resource(ResourceFactory, '/factory', '/factory/all')
-api_client_db.add_resource(ResourceDevice, '/device', '/device/all')
-api_client_db.add_resource(ResourceTestdataCloud, '/testdatacloud', '/testdatacloud/all')
+api_db.add_resource(ResourceFactoryRaw, '/raw/factory', '/raw/factory/all')
+api_db.add_resource(ResourceDeviceRaw, '/raw/device', '/raw/device/all')
+api_db.add_resource(ResourceTestdataCloudRaw, '/raw/testdatacloud', '/raw/testdatacloud/all')
+api_db.add_resource(ResourceTestdataCloudStatistic, '/statistic/testdatacloud/<string:metric>')
 
